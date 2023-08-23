7819268fdefef969d148983582a0c1cc32fcf8a0
AndreiBriceag <andreibriceag17@gmail.com>
2023-08-23 09:58:01 +0300

    Fix for encrypt/decrypt processes

--- python/encrypt/encrypt.py
+++ python/encrypt/encrypt.py
@@ -1,9 +1,9 @@
 from cryptography.fernet import Fernet
 
-# generate key
-key = Fernet.generate_key()
-with open('python/encrypt/filekey.key', 'wb') as filekey:
-    filekey.write(key)
+# generate key; COMMENT THIS TO NOT GENERATE NEW KEY
+# key = Fernet.generate_key()
+# with open('python/encrypt/filekey.key', 'wb') as filekey:
+#     filekey.write(key)
 
 # open key
 with open('python/encrypt/filekey.key', 'rb') as filekey:
@@ -12,19 +12,19 @@ with open('python/encrypt/filekey.key', 'rb') as filekey:
 # using generated key on line 4
 fernet = Fernet(key)
 
-# open original file to encrypt
-with open('python/encrypt/important.csv', 'rb') as file:
-    original = file.read()
+# COMMENT ALL BELLOW THIS LINE TO DECRYPT ONLY
+# # open original file to encrypt
+# with open('python/encrypt/important.csv', 'rb') as file:
+#     original = file.read()
 
-# encrypt the file
-encrypted = fernet.encrypt(original)
+# # encrypt the file
+# encrypted = fernet.encrypt(original)
 
-# open the file in write mode and write encrypted data
-with open('python/encrypt/important.csv', 'wb') as encryptedFile:
-    encryptedFile.write(encrypted)
+# # open the file in write mode and write encrypted data
+# with open('python/encrypt/important.csv', 'wb') as encryptedFile:
+#     encryptedFile.write(encrypted)
 
 # COMMENT ALL BELLOW THIS LINE TO ENCRYPT ONLY
-
 # using the key
 fernet = Fernet(key)
  
